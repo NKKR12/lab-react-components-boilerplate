e0590d9949378faa08d4737175fd58088e536696
Nikhil Kumar <nkkr9538@gmail.com>
2024-03-04 12:36:15 +0530

    lab-react-components

---
+++ src/components/HeaderTwo.jsx
@@ -0,0 +1,17 @@
+export default function HeaderTwo(imageData){
+    console.log(imageData)
+   
+   return(
+<div>
+       {
+         imageData.imageData.map(image=>{
+           return(
+             <div key={image.id} className="column">
+               <img src={image.img} alt=""></img>
+             </div>
+           )
+         })
+       }
+     </div>
+     )
+}
